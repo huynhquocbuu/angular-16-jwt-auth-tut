4b7d764101a569b5c1921f02c8e652ed65818c8c
BuuHQ <BuuHQ@tpb.vn>
2023-09-13 16:27:41 +0700

    1st

---
+++ src/app/_service/auth.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    var httpBody = 
+      {
+        username,
+        password,
+      };
+    return this.http.post(
+      AUTH_API + 'signin',
+      httpBody,
+      httpOptions
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    var httpBody = 
+      {
+        username,
+        email,
+        password,
+      };
+    return this.http.post(
+      AUTH_API + 'signup',
+      httpBody,
+      httpOptions
+    );
+  }
+  
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'signout', { }, httpOptions);
+  }
+
+}
